fbd2d315d262808a94e935b1f45846f78dcc312f
Jerry De Jesus <jdejesus7@live.com>
2021-10-14 15:06:10 -0400

    Completed Ghibli Review App

--- script.js
+++ script.js
@@ -11,18 +11,35 @@ fetch("https://ghibliapi.herokuapp.com/films")
             let newOption = document.createElement("option");
             newOption.textContent = movieTitle[0].toUpperCase() + movieTitle.slice(1);
             newOption.value = movieTitle;
+            newOption.id = movie.id;
             select.append(newOption);
         }
+
+    select.addEventListener("change", (e)=>{
+        e.preventDefault();
+        let chosenMovie = e.target.value;
+        let display = document.querySelector("#display-info");
+        for(let i=0; i < data.length;i++){
+            if(chosenMovie === data[i].title){
+                display.innerHTML = `
+            <h3>${data[i].title}</h3>
+            <p>${data[i].release_date}</p>
+            <p>${data[i].description}</p>`
+            }
+        }
+    })
+
+
         let form = document.querySelector("form");
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
-        let chosenMovie = e.target["movie-select"].value;
-        let display = document.querySelector("#display-info");
-        display.innerHTML = `
-            <h3>${chosenMovie}</h3>
-            <p></p>
-            <p></p>
-        `
+        let movieTitle = document.querySelector("h3");
+        let newReview = document.createElement("li");
+        let reviewEntry = document.querySelector("#reviewEntry");
+        newReview.innerHTML = `<b>${movieTitle.textContent}: </b>${reviewEntry.value}`;
+        reviewEntry.value = "";
+        let reviewList = document.querySelector("#review-list");
+        reviewList.append(newReview);
     })
     })
 
